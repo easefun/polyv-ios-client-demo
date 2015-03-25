6d807a013d7dfe808e59580eaad192f9136760ee
appwallets <junjing@gmail.com>
2015-03-25 15:41:12 +0800

    增加描述

--- polyvSDK/PolyvPlayerDemoViewController.m
+++ polyvSDK/PolyvPlayerDemoViewController.m
@@ -42,29 +42,44 @@
     
     [downloder deleteVideo];
 }
-
+/**
+ 点击播放按钮执行创建一个PolyvPlayerViewController实例，设置播放器大小为320x180，位置在左上角0，0坐标
+ */
 - (IBAction)playAction:(id)sender {
+    //用vid初始化播放器
     player = [[PolyvPlayerViewController alloc] initWithVid:vid delegate:self];
     [player setFrame: CGRectMake(0, 0, 320, 180)];
     [player setPlayerControlStyle:MPMovieControlStyleDefault];
     [self.view addSubview:player.view];
-    
+    //启动播放器
     [player startPlayer];
 }
+
+/**
+ 播放器切换另外一个视频
+ */
 - (IBAction)switchVid:(id)sender {
     [player stopPlayer];
     [player changeVideo:@"sl8da4jjbx2d77fa6b3588b379f33289_s"];
     [player startPlayer];
 }
-
+/**
+ 暂停播放器
+ **/
 - (IBAction)pauseAction:(id)sender {
     [player pausePlayer];
 }
-
+/**
+ 启动下载器
+ **/
 - (IBAction)downloadAction:(id)sender {
     [downloder download];
 }
 
+/**
+ 播放本地视频，要事先下载好视频之后，如果本地文件不存在则无法播放
+ [Helper getDownloadFilePath:vid]用vid获取到本地文件所在路径
+ */
 - (IBAction)playLocalAction:(id)sender {
     player = [[PolyvPlayerViewController alloc] initPlayerWithLocalPath:[Helper getDownloadFilePath:vid] encoded:YES delegate:self];
     NSLog(@"play local file:%@",[Helper getDownloadFilePath:vid]);
@@ -78,6 +93,9 @@
     [player startPlayer];
 
 }
+/**
+ 按了全屏按钮
+ */
 
 - (IBAction)fullscreenAction:(id)sender {
     [player setFullscreen:YES animated:YES];
@@ -122,16 +140,19 @@
 
 }
 
-
+/**
+ 初始化viewcontroller
+ **/
 
 - (void)viewDidLoad
 {
+    //演示用的vid
     
     vid = @"sl8da4jjbxa3c15f99bc37545693f7f9_s";
-
-
-    downloder = [[DownloadHelper alloc] initWithVid:vid encode:true delegate:self];
     
+    //初始化下载器，encode为true，下载的视频到本地的时候做加密
+    downloder = [[DownloadHelper alloc] initWithVid:vid encode:true delegate:self];
+    //设置下载目录属性
     [downloder addSkipBackupAttributeToDownloadedVideos];
     //downloder.delegate = self;
     
