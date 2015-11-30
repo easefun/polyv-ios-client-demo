9c77e9e6f1cbeab3406d5852bb6db2518812f5e7
appwallets <junjing@gmail.com>
2015-11-30 16:09:50 +0800

    bug fixed

--- polyvSDK/AppDelegate.m
+++ polyvSDK/AppDelegate.m
@@ -56,7 +56,6 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      [[PolyvSettings sharedInstance]  reloadSettings];
-    NSLog(@"reload");
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
