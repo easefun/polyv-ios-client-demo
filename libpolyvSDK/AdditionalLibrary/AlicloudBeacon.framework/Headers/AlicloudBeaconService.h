005e657933064fabc896717d6b5b65a29ea3d09c
ftao <fft1026@163.com>
2018-12-13 17:16:08 +0800

    # SDK 版本更新为 1.6.0 - 更新 AlicloudHttpDNS 版至为 1.6.10 - viewlog 接口修正使用 urlEncode 编码

---
+++ libpolyvSDK/AdditionalLibrary/AlicloudBeacon.framework/Headers/AlicloudBeaconService.h
@@ -0,0 +1,41 @@
+//
+//  AlicloudBeaconService.h
+//  AlicloudBeacon
+//
+//  Created by junmo on 2017/7/6.
+//  Copyright © 2017年 junmo. All rights reserved.
+//
+
+#ifndef AlicloudBeaconService_h
+#define AlicloudBeaconService_h
+
+@interface AlicloudBeaconConfiguration : NSObject
+
+- (instancetype)initWithData:(NSData *)data;
+- (id)getConfigureItemByKey:(NSString *)key;
+
+@end
+
+typedef void (^AlicloudBeaconCallbackHandler)(BOOL res, NSError *error);
+
+@interface AlicloudBeaconService : NSObject
+- (instancetype)initWithAppKey:(NSString *)appKey
+                     appSecret:(NSString *)appSecret
+                    SDKVersion:(NSString *)SDKVersion
+                         SDKID:(NSString *)SDKID;
+
+- (instancetype)initWithAppKey:(NSString *)appKey
+                     appSecret:(NSString *)appSecret
+                    SDKVersion:(NSString *)SDKVersion
+                         SDKID:(NSString *)SDKID
+                     extension:(NSDictionary *)extension;
+
+- (void)enableLog:(BOOL)enabled;
+- (BOOL)isLogEnabled;
+
+- (void)getBeaconConfigStringByKey:(NSString *)key
+           completionHandler:(void(^)(NSString *result, NSError *error))completionHandler;
+
+@end
+
+#endif /* AlicloudBeaconService_h */
--- libpolyvSDK/AdditionalLibrary/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
+++ libpolyvSDK/AdditionalLibrary/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.4.0";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.6.10";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsServiceProvider.h"
